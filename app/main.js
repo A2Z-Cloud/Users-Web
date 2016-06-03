a2b458ccdfabcb5206cfc4dbb886ec016f0b1d9a
jamesstidard <jamesstidard@gmail.com>
2016-06-03 14:53:46 +0100

    sign out redirects

--- app/main.js
+++ app/main.js
@@ -35,11 +35,25 @@ System.import(control_url).then(({Control}) => {  // eslint-disable-line no-unde
                      message: "Cannot connect to server.",
                  }))
         },
+        computed: {
+            sign_out_url() {
+                // Take the user to the A2Z Auth with a redirect to the
+                // auth sign in page after they sign out.
+                // Additionally after they sign in again have the next
+                // parameter be set back to this url
+                const delimiter   = this.$router.mode === 'hash' ? '/#!' : ''
+                const current_url = window.location.protocol + '//' + window.location.host + delimiter + this.$route.path
+                const sign_in_url = this.auth_client_url
+                const sign_in_url_return = sign_in_url + '?next=' + encodeURIComponent(current_url)
+
+                return this.auth_client_url + '/sign-out?next=' + encodeURIComponent(sign_in_url_return)
+            },
+        },
         vuex: {
             getters:  {
                 user: state => state.user,
                 ws_ready: state => state.auth_client_url && state.ws_status === 'open',
-                sign_out_url: state => state.auth_client_url + '/sign-out',
+                auth_client_url: state => state.auth_client_url,
             },
         },
         events: {
