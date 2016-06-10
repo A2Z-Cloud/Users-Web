aeef65437df1c00c05d56deb326442c3c36daae4
jamesstidard <jamesstidard@gmail.com>
2016-06-10 11:11:29 +0100

    const update

--- app/consts.js
+++ app/consts.js
@@ -1,4 +1,4 @@
 export const debug                  = true
-export const control_url            = 'http://localhost:8888/control.js'
+export const control_url            = 'http://localhost:9001/control.js'
 export const email_confirmation_url = 'http://users.a2zcloud.local/#!/confirm-email'
 export const invite_url             = 'http://users.a2zcloud.local/#!/invite'
