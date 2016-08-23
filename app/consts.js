aba7f97beca92ba1d62d10684d6bafe673ee955d
Oliver Bunyan <Dashb@me.com>
2016-08-23 18:23:25 +0100

    fixing bad commit

--- app/consts.js
+++
@@ -1,5 +0,0 @@
-export const debug                  = true
-export const hash_routing           = false
-export const control_url            = 'http://localhost:9001/control.js'
-export const email_confirmation_url = 'http://users.a2zcloud.local/confirm-email'
-export const invite_url             = 'http://users.a2zcloud.local/invite'
