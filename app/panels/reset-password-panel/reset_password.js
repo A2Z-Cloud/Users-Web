77d25818e0f278da3d71c914294d92781e841b97
jamesstidard <jamesstidard@gmail.com>
2016-10-12 14:09:57 +0100

    extra password verification

--- app/panels/reset-password-panel/reset_password.js
+++ app/panels/reset-password-panel/reset_password.js
@@ -2,6 +2,8 @@ import './reset_password.css!'
 import tmpl from './reset_password.html!text'
 import Vue from 'vue'
 
+import { password_errors } from 'app/utils/validation'
+
 import { reset_password } from 'app/vuex/actions'
 
 
@@ -14,11 +16,21 @@ export default Vue.extend({
         error: null,
     }),
     computed: {
+        new_password_errors() {
+            return password_errors(this.new_password)
+        },
+        invalid_password_confirmation() {
+            return this.form_filled
+                && this.new_password_confirm !== this.new_password
+        },
         enable_button() {
-            return this.form_filled && this.new_password == this.new_password_confirm
+            return this.form_filled
+                && !this.new_password_errors.length
+                && !this.invalid_password_confirmation
         },
         form_filled() {
-            return this.new_password && this.new_password_confirm
+            return this.new_password
+                && this.new_password_confirm
         },
         field_type() {
             return this.visible ? 'text' : 'password'
@@ -38,12 +50,9 @@ export default Vue.extend({
                 const token = this.$route.query.token
                 const new_password = this.new_password
                 return this.reset_password({token, new_password})
-                           .then(r => (console.log('success')))
+                           .then(console.log)
                            .catch(e => (this.error=e.message))
             }
-        },
-        toggle_visibility() {
-            this.visible = !this.visible
         },
     },
     events: {
